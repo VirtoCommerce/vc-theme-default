3726f0b22d1cfe85efbcf81ff234709ce51e4653
Valeriy Krashenko <vk@virtoway.com>
2017-10-26 18:18:55 +0200

    fix subscribtion

---
+++ assets/js/subscribtion.js
@@ -0,0 +1,7 @@
+﻿var storefrontApp = angular.module('storefrontApp');
+storefrontApp.controller('subscribtionController', ['$scope', 'feedbackService', function ($scope, feedbackService) {
+    $scope.submit = function () {
+        feedbackService.postFeedback({email:$scope.email}).then(function (resp) {
+        })
+    }
+}]);
