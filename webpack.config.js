9c18a4537dc1c396994b0b2339235da55a389daa
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2018-12-07 17:56:58 +0200

    Union configs to single

--- webpack.config.js
+++ webpack.config.js
@@ -9,40 +9,34 @@ const rootPath = path.resolve(__dirname, 'assets/dist');
 
 module.exports = [
     {
-        entry: ['./src/js/checkout-scripts.js', ...glob.sync('./assets/js/common-components/*.js'), ...glob.sync('./assets/js/checkout/*.js')],
+        entry: {
+            'checkout-scripts': [
+                './src/js/checkout-scripts.js', 
+                ...glob.sync('./assets/js/common-components/*.js'), 
+                ...glob.sync('./assets/js/checkout/*.js')
+            ],
+            'scripts': [
+                './src/js/scripts.js', 
+                ...glob.sync('./assets/js/*.js', { ignore: './assets/js/app.js', nosort: true }), 
+                ...glob.sync('./assets/js/products-compare/*.js')
+            ],
+            'account-scripts': [
+                './src/js/account-scripts.js', 
+                ...glob.sync('./assets/js/products-compare/*.js'), 
+                ...glob.sync('./assets/js/common-components/*.js'), 
+                ...glob.sync('./assets/js/account/*.js'),
+                ...glob.sync('./assets/js/lists/*.js')
+            ],
+            'scripts_dependencies': './src/js/vendor.js',
+            'styles': './src/css/styles.css',
+            'checkout-styles': './src/css/checkout-styles.css',
+            'account-styles': './src/css/account-styles.css'
+        },
         output: {
             path: rootPath,
-            filename: 'checkout-scripts.js',
+            filename: '[name].js',
             publicPath: 'assets/dist/'
         },
-        plugins: [
-            new CleanWebpackPlugin(rootPath, { verbose: true }),
-            new webpack.ProvidePlugin({
-                $: 'jquery',
-                jQuery: 'jquery',
-                'window.jQuery': 'jquery',
-                _: 'underscore'
-            })
-        ],
-        resolve: {
-            alias: {
-                Assets: path.resolve(__dirname, 'assets'),
-                Vendor: path.resolve(__dirname, 'node_modules')
-            }
-        }
-    },
-    {
-        entry: [
-            './src/js/scripts.js', 
-            ...glob.sync('./assets/js/*.js', { ignore: './assets/js/app.js', nosort: true }), 
-            ...glob.sync('./assets/js/products-compare/*.js')
-        ],
-        output: {
-            path: rootPath,
-            filename: 'scripts.js',
-            publicPath: 'assets/dist/'
-        },
-        devtool: 'eval-source-map',
         module: {
             rules: [
                 {
@@ -52,86 +46,7 @@ module.exports = [
                 {
                     test: require.resolve('ideal-image-slider/ideal-image-slider'),
                     use: 'exports-loader?IdealImageSlider'
-                }
-            ]
-        },
-        resolve: {
-            alias: {
-                Assets: path.resolve(__dirname, 'assets'),
-                modernizr$: path.resolve(__dirname, ".modernizrrc.js")
-            }
-        },
-        plugins: [
-            new webpack.ProvidePlugin({
-                $: 'jquery',
-                jQuery: 'jquery',
-                'window.jQuery': 'jquery',
-                IdealImageSlider: 'ideal-image-slider/ideal-image-slider',
-                _: 'underscore'
-            })
-        ]
-    },
-    {
-        entry: [
-            './src/js/account-scripts.js', 
-            ...glob.sync('./assets/js/products-compare/*.js'), 
-            ...glob.sync('./assets/js/common-components/*.js'), 
-            ...glob.sync('./assets/js/account/*.js'),
-            ...glob.sync('./assets/js/lists/*.js')
-        ],
-        output: {
-            path: rootPath,
-            filename: 'account-scripts.js',
-            publicPath: 'assets/dist/'
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.modernizrrc\.js$/,
-                    loader: 'webpack-modernizr-loader'
                 },
-            ]
-        },
-        resolve: {
-            alias: {
-                Assets: path.resolve(__dirname, 'assets'),
-                modernizr$: path.resolve(__dirname, ".modernizrrc.js")
-            }
-        },
-        plugins: [
-            new webpack.ProvidePlugin({
-                $: 'jquery',
-                jQuery: 'jquery',
-                'window.jQuery': 'jquery'
-            }),
-            new webpack.ProvidePlugin({
-                _: 'underscore'
-            })
-        ]
-    },
-    {
-        entry: './src/js/vendor.js',
-        output: {
-            path: rootPath,
-            filename: 'scripts_dependencies.js',
-            publicPath: 'assets/dist/'
-        },
-        plugins: [
-            new webpack.ProvidePlugin({
-                $: 'jquery',
-                jQuery: 'jquery',
-                'window.jQuery': 'jquery'
-            })
-        ]
-    },
-    {
-        entry: {
-            'styles': './src/css/styles.css',
-            'checkout-styles': './src/css/checkout-styles.css',
-            'account-styles': './src/css/account-styles.css'
-        },
-        module: {
-            rules: [
                 {
                     test: /\.(jpe?g|png|gif|svg)$/i,
                     loader: "file-loader",
@@ -149,11 +64,15 @@ module.exports = [
                 }
             ]
         },
-        output: {
-            path: rootPath,
-            publicPath: 'assets/dist/'
-        },
         plugins: [
+            new CleanWebpackPlugin(rootPath, { verbose: true }),
+            new webpack.ProvidePlugin({
+                $: 'jquery',
+                jQuery: 'jquery',
+                'window.jQuery': 'jquery',
+                _: 'underscore',
+                IdealImageSlider: 'ideal-image-slider/ideal-image-slider',
+            }),
             new FixStylesOnlyEntriesPlugin(),
             new MinCssExtractPlugin({
                 filename: "[name].css"
@@ -161,7 +80,9 @@ module.exports = [
         ],
         resolve: {
             alias: {
-                Assets: path.resolve(__dirname, 'assets')
+                Assets: path.resolve(__dirname, 'assets'),
+                Vendor: path.resolve(__dirname, 'node_modules'),
+                modernizr$: path.resolve(__dirname, ".modernizrrc.js")
             }
         }
     }
