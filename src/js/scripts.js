bb01fdf884c0993bccde7d6e02b81d7d44e07c80
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2018-12-07 17:56:12 +0200

    Fixed typo

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,7 +1,7 @@
 import 'modernizr';
 import 'Assets/js/app.js';
 import 'Assets/interactor.js';
-import * as IdealImageSlider from'ideal-image-slider/ideal-image-slider';
+import * as IdealImageSlider from 'ideal-image-slider/ideal-image-slider';
 window.IdealImageSlider = IdealImageSlider;
 import 'ideal-image-slider/extensions/bullet-nav/iis-bullet-nav';
 import 'ideal-image-slider/extensions/captions/iis-captions';
