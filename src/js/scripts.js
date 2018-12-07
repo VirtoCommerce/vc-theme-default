300ab78d682bf913bcb033da9ca6365bcb503c4c
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2018-12-07 13:06:43 +0200

    Added semicolon

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,4 +3,4 @@ import 'Assets/interactor.js';
 import 'ideal-image-slider/ideal-image-slider';
 import 'ideal-image-slider/extensions/bullet-nav/iis-bullet-nav';
 import 'ideal-image-slider/extensions/captions/iis-captions';
-import 'Assets/js/lists/add-to-list.js'
+import 'Assets/js/lists/add-to-list.js';
