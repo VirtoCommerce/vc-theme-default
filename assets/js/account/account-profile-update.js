0c169634b29890222afd2bcddb4dfbf1b54bbafc
Ilya <ilya.ivanov6@gmail.com>
2019-02-26 15:38:46 +0200

    added forms for phone number,verifying phone number; changed template for profile update

--- assets/js/account/account-profile-update.js
+++ assets/js/account/account-profile-update.js
@@ -1,36 +1,54 @@
-angular.module('storefront.account')
-.component('vcAccountProfileUpdate', {
+angular.module("storefront.account").component("vcAccountProfileUpdate", {
     templateUrl: "themes/assets/js/account/account-profile-update.tpl.liquid",
     bindings: {
-        $router: '<'
+        $router: "<"
     },
     require: {
-        accountManager: '^vcAccountManager'
+        accountManager: "^vcAccountManager"
     },
-    controller: ['storefrontApp.mainContext', '$scope', 'loadingIndicatorService', function (mainContext, $scope, loader) {
-        var $ctrl = this;
-        $ctrl.loader = loader;
-        
-        $scope.$watch(
-            function () { return mainContext.customer; },
-            function (customer) {
-                $ctrl.customer = customer;
-                if (customer) {
-                    if (customer.isContract) {
-                        $ctrl.$router.navigate(['Orders']);
-                    }
-                    $ctrl.changeData =
-                    {
-                        firstName: customer.firstName,
-                        lastName: customer.lastName,
-                        email: customer.email
-                    };
-                }
-            });
+    controller: [
+        "storefrontApp.mainContext",
+        "$scope",
+        "loadingIndicatorService",
+        function(mainContext, $scope, loader) {
+            var $ctrl = this;
+            $ctrl.loader = loader;
 
-        $ctrl.submit = function () {
-            // no validation
-            $ctrl.accountManager.updateProfile($ctrl.changeData);
-        };
-    }]
+            $scope.$watch(
+                function() {
+                    return mainContext.customer;
+                },
+                function(customer) {
+                    $ctrl.customer = customer;
+                    if (customer) {
+                        if (customer.isContract) {
+                            $ctrl.$router.navigate(["Orders"]);
+                        }
+                        $ctrl.changeData = {
+                            firstName: customer.firstName,
+                            lastName: customer.lastName,
+                            email: customer.email
+                        };
+                    }
+                }
+            );
+
+            $ctrl.submit = function() {
+                // no validation
+                $ctrl.accountManager.updateProfile($ctrl.changeData);
+            };
+
+            $ctrl.changePhoneNumber = function() {
+                $ctrl.accountManager.changePhoneNumber($ctrl.phoneNumber);
+            };
+
+            $ctrl.deletePhoneNumber = function() {
+                $ctrl.accountManager.deletePhoneNumber();
+            };
+
+            $ctrl.changeTwoFactorAuth = function() {
+                $ctrl.accountManager.changeTwoFactorAuth();
+            };
+        }
+    ]
 });
