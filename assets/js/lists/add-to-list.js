ef96dbc5c1141a747fccc2d1629b82faa38e9bfd
Andrey Kozlov <yecli.github@gmail.com>
2020-01-27 15:54:45 +0500

    VP-1229: Use component routing
    - Used AngularJS component rouring for passing listName to the lists component;
    - Removed redundant services from account-lists.js controllers;

--- assets/js/lists/add-to-list.js
+++ assets/js/lists/add-to-list.js
@@ -1,6 +1,6 @@
 var storefrontApp = angular.module('storefrontApp');
 
-storefrontApp.controller('recentlyAddedListItemDialogController', ['$scope', '$window', '$uibModalInstance', 'dialogData', 'listService', '$translate', '$localStorage', function ($scope, $window, $uibModalInstance, dialogData, listService, $translate, $localStorage) {
+storefrontApp.controller('recentlyAddedListItemDialogController', ['$scope', '$window', '$uibModalInstance', 'dialogData', 'listService', '$translate', function ($scope, $window, $uibModalInstance, dialogData, listService, $translate) {
     $scope.availableLists = [];
     $scope.selectedList = {};
     $scope.dialogData = dialogData;
@@ -26,7 +26,6 @@ storefrontApp.controller('recentlyAddedListItemDialogController', ['$scope', '$w
     };
 
     $scope.redirect = function (url) {
-        $localStorage['selectedPrivateListName'] = $scope.selectedList.name;
         $window.location = url;
     };
 
