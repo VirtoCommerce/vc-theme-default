0bf817842471ce9da13e9898b8b1315034f3e60c
Dmitry Pushnitsa <2745590@gmail.com>
2020-05-29 06:57:33 +0200

    VP-1947: Fix order frequency reset on checkout (#149)
    * Add equal case comparing

--- assets/js/checkout/checkout.js
+++ assets/js/checkout/checkout.js
@@ -59,7 +59,12 @@ angular.module(moduleName, ['credit-cards', 'angular.filter'])
                         $scope.checkout.billingAddressEqualsShipping = cart.hasPhysicalProducts && !angular.isObject($scope.checkout.payment.billingAddress);
 
                         $scope.checkout.canCartBeRecurring = $scope.customer.isRegisteredUser && _.all(cart.items, function(x) { return !x.isReccuring });
-                        $scope.checkout.paymentPlan = cart.paymentPlan && _.findWhere($scope.checkout.availablePaymentPlans, { intervalCount: cart.paymentPlan.intervalCount, interval: cart.paymentPlan.interval }) ||
+
+                        _.each($scope.checkout.availablePaymentPlans, function(value) {
+                            value.interval = value.interval.toLowerCase();
+                        });
+
+                        $scope.checkout.paymentPlan = cart.paymentPlan && _.findWhere($scope.checkout.availablePaymentPlans, { intervalCount: cart.paymentPlan.intervalCount, interval: cart.paymentPlan.interval.toLowerCase() }) ||
                             _.findWhere($scope.checkout.availablePaymentPlans, { intervalCount: 1, interval: 'months' });
                     }
                     $scope.validateCheckout($scope.checkout);
