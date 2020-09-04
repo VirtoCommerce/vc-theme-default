5cb28c58a15500ad884fd2a7e856fc4d43e2d627
Andrei Kostyrin <kostyrin2@gmail.com>
2020-09-04 11:50:22 +0200

    #VP-2838 remove calculate Total

--- assets/js/checkout/checkout.js
+++ assets/js/checkout/checkout.js
@@ -213,7 +213,6 @@ angular.module(moduleName, ['credit-cards', 'angular.filter'])
                 $scope.checkout.payment.paymentGatewayCode = paymentMethod.code;
                 $scope.checkout.payment.amount = angular.copy($scope.checkout.cart.total);
                 $scope.checkout.payment.amount.amount += paymentMethod.totalWithTax.amount;
-                $scope.checkout.payment.total = $scope.checkout.payment.amount;
 
                 updatePayment($scope.checkout.payment);
             };
